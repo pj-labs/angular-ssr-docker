f7a340332da1e8f33919473d7b14dd5f5cc1314a
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-05-05 00:33:49 -0400

    feat(core): upgrades to 16.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
